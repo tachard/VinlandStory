ce47ba69f1ea1815a9a7c87f9a0b54e10cf06bdd
tachard <tachard@ensc.fr>
2022-04-14 16:23:39 +0200

    First unit tests

---
+++ VinlandStory/UnitTests/TestsResources.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using VinlandStory;
+
+namespace VinlandStory.Tests
+{
+    [TestClass()]
+    public class TestsResources
+    {
+        [TestMethod()]
+        public void ResourcesTest()
+        {
+            int food = 64;
+            int stone = 78;
+            int wood = 109;
+
+            Resources test = new Resources(wood, stone, food);
+            Assert.AreEqual(new Resources(wood, stone, food).ToString(), test.ToString());
+        }
+
+        [TestMethod()]
+        public void ResourcesToStringTest()
+        {
+            int food = 64;
+            int stone = 78;
+            int wood = 109;
+
+            Resources test = new Resources(wood, stone, food);
+            Assert.AreEqual("Bois : 109\tPierre : 78\tNourriture : 64", test.ToString());
+        }
+    }
+}
---
+++ VinlandStory/UnitTests/TestsTiles.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VinlandStory.Buildings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinlandStory.Tests
+{
+    [TestClass()]
+    public class TestsTile
+    {
+        [TestMethod()]
+        public void BuildingTileTest()
+        {
+            Random r = new Random();
+            Longhouse l = new Longhouse(3, 4, new Resources(0, 0, 0));
+
+            BuildingTile tile = new BuildingTile(r, l);
+            Assert.AreEqual(new Resources(0,0,0).ToString(),tile.Available.ToString());
+            Assert.AreEqual(3, tile.X);
+            Assert.AreEqual(4, tile.Y);
+            Assert.AreEqual(l, tile.Build);
+        }
+
+        [TestMethod()]
+        public void PrintBuildingTileTest()
+        {
+            Random r = new Random();
+            Longhouse l = new Longhouse(3, 4, new Resources(0, 0, 0));
+
+            BuildingTile tile = new BuildingTile(r, l);
+            tile.PrintTile();
+
+            Assert.AreEqual(ConsoleColor.Gray, Console.ForegroundColor);
+            Assert.AreEqual(ConsoleColor.Black, Console.BackgroundColor);
+        }
+    }
+}
